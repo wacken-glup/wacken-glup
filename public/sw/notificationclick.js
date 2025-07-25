e3ba4d3f7bc09eec9c73ba591a47fb26f25060a5
aimok04 <aimok04@mailbox.org>
2025-07-26 01:02:47 +0200

    fix(sw/notificationclick.js): removed action check

--- public/sw/notificationclick.js
+++ public/sw/notificationclick.js
@@ -3,21 +3,19 @@ self.addEventListener('notificationclick', onNotificationClick);
 async function onNotificationClick(e) {
     e.notification.close() 
 
-    if(e.action == "url") {
-        e.waitUntil(
-            clients.matchAll({ type: "window" }).then((clientsArr) => {
-                const windowToFocus = clientsArr.find(
-                    (windowClient) => windowClient.url === e.notification.data.url,
-                )
+    e.waitUntil(
+        clients.matchAll({ type: "window" }).then((clientsArr) => {
+            const windowToFocus = clientsArr.find(
+                (windowClient) => windowClient.url === e.notification.data.url,
+            )
 
-                if(windowToFocus) {
-                    windowToFocus.focus();
-                }else{
-                    clients
-                        .openWindow(e.notification.data.url)
-                        .then((windowClient) => (windowClient ? windowClient.focus() : null));
-                }
-            })
-        )
-    }
+            if(windowToFocus) {
+                windowToFocus.focus();
+            }else{
+                clients
+                    .openWindow(e.notification.data.url)
+                    .then((windowClient) => (windowClient ? windowClient.focus() : null));
+            }
+        })
+    )
 }
